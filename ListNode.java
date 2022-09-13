f6583d33e613035c7e081238478c37be7e96c646
dksqudgns <97275688+byounghunAHnNN@users.noreply.github.com>
2022-09-13 21:04:03 +0900

    leetcode

---
+++ ListNode.java
@@ -0,0 +1,51 @@
+package study1;
+
+  public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     
+  }
+class Solution1 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ret = new ListNode();
+        ListNode target = ret; 
+        int carry = 0;
+        while(l1!=null || l2!=null) {
+            int val1 = 0;
+            int val2 = 0;
+            
+            if(l1!=null) {
+                val1=l1.val;
+            }
+            if(l2!=null) {
+                val2=l2.val;
+            }
+            int sum =val1+val2+carry;
+            
+            target.val = sum%10;
+            
+            if(sum>=10) {
+                carry = 1;
+            } else {
+                carry =0;
+            }
+            if(l1!=null) {
+                l1=l1.next;
+            }
+            if(l2!=null) {
+                l2=l2.next;
+            }
+            if(l1!=null || l2!=null) {
+                target.next = new ListNode();
+                target = target.next;
+            }
+        }
+        if(carry ==1 ) {
+            target.next = new ListNode(1);
+        }
+        return ret;
+    }
+}   
