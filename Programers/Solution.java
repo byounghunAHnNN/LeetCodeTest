8b8059859868262607ca1f64a0ea0ec19f58f8c4
Byounghun <97275688+byounghunAHnNN@users.noreply.github.com>
2024-05-13 22:47:23 +0900

    java 알고리즘 기초

---
+++ Programers/Solution.java
@@ -0,0 +1,12 @@
+package Programers;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str= sc.next();
+       System.out.println(str);
+       sc.close();
+    }
+}
